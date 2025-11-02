09d950002bdafe403c9937be4777f471f0130d5d
basilioarth <basilio.arth@gmail.com>
2025-11-02 11:29:34 -0300

    Aplicação do módulo

---
+++ ftr-pos-360-deployments-devops-main/server/src/routes/health-check.ts
@@ -0,0 +1,9 @@
+import type { FastifyInstance } from "fastify";
+import { log } from "../infra/logger"
+
+export async function healthCheckRoute(app: FastifyInstance) {
+  app.get('/health', async (request, reply) => {
+    log.info('Acessei a rota e deu certo');
+    await reply.status(200).send({ message: 'Acessando o preview!' })
+  })
+}
---
+++ ftr-pos-360-deployments-devops-main/server/src/server.ts
@@ -0,0 +1,42 @@
+import { fastify } from 'fastify'
+import { fastifyCors } from '@fastify/cors'
+import { uploadImageRoute } from './routes/upload-image'
+import { fastifyMultipart } from '@fastify/multipart'
+//import secret from './infra/secret'
+import { log } from './infra/logger'
+//import { GetParameterCommand, SSMClient } from '@aws-sdk/client-ssm'
+//import { DecryptCommand, KMSClient } from '@aws-sdk/client-kms'
+//import { GetSecretValueCommand, SecretsManagerClient } from '@aws-sdk/client-secrets-manager'
+
+//const ssm = new SSMClient({ region: 'us-east-2' })
+//const kms = new KMSClient({ region: 'us-east-2' })
+//const secretManager = new SecretsManagerClient({ region: 'us-east-2' })
+
+const server = fastify()
+
+server.register(fastifyCors, {
+  origin: '*',
+})
+
+server.register(fastifyMultipart)
+server.register(uploadImageRoute)
+
+server.listen({ port: 3333, host: '0.0.0.0' }).then(async () => {
+  //const values = await secret.read('/secret/data/widget-server-stg')
+  //console.log(values.data.data)
+  //const value = await ssm.send(new GetParameterCommand(
+  //  { Name: 'CLOUDFLARE_ACCESS_KEY_ID', WithDecryption: true }
+  //))
+  //console.log(value.Parameter?.Value)
+  //const secret = await secretManager.send(new GetSecretValueCommand({ SecretId: 'stg/widget-server' }))
+  //console.log(JSON.parse(secret.SecretString))
+  //if (value.Parameter?.Value) {
+  //  const command = new DecryptCommand({
+  //    CiphertextBlob: Buffer.from(value.Parameter.Value, 'base64')
+  //})
+  //const commandResult = await kms.send(command)
+  //const result = new TextDecoder().decode(commandResult.Plaintext)
+  //console.log(result)
+  //}
+  log.info('HTTP server running!!!')
+})
